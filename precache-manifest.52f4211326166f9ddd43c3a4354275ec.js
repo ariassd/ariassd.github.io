f51350fdddcc5863933f814c445d8357671921ea
Luis Arias <ariassd@gmail.com>
2021-05-05 14:30:25 -0600

    Updates

---
+++ precache-manifest.52f4211326166f9ddd43c3a4354275ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e260f1d115e01ea8df78684db80cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4d53bb5ba9ac4e8088f",
+    "url": "/static/css/2.0b5d44dd.chunk.css"
+  },
+  {
+    "revision": "e4d53bb5ba9ac4e8088f",
+    "url": "/static/js/2.24d6e62c.chunk.js"
+  },
+  {
+    "revision": "b8d7b2a6fdc6e0ab1fc98c0e76656d66",
+    "url": "/static/js/2.24d6e62c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c9d6465f69205388d1",
+    "url": "/static/js/main.c5c71fae.chunk.js"
+  },
+  {
+    "revision": "284a543388774a19089d",
+    "url": "/static/js/runtime-main.c6496638.js"
+  }
+]);
